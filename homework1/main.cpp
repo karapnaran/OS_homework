2c018347fc2347bc7592d0923aa7ebb360cfc7b6
karapnaran <36893642+karapnaran@users.noreply.github.com>
2020-09-27 15:14:52 +0400

    Add Bogo sort

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <algorithm>
+#include <random>
 #include "complex.h"
 using namespace std;
+
+// Checks if the array is sorted
+bool isSorted(Complex a[], int n)
+{
+    for(int i=0; i<n-1; ++i)
+        if (a[i] > a[i+1]) return false;
+    return true;
+}
+
+// Generates permuatations of the array
+void shuffle(Complex a[], int n)
+{
+    for (int i=0; i < n; i++)
+        swap(a[i], a[rand()%n]);
+}
+
+// Sorts the array using Bogo sort
+void bogosort(Complex a[], int n)
+{
+    // if array is not sorted then shuffle the array again
+    while (!isSorted(a,n))
+        shuffle(a,n);
+}
 
 int main()
 {
@@ -13,10 +37,9 @@ int main()
         cin>>real>>imaginary;
         a[i]=Complex(real,imaginary);
     }
-    sort(a,a+N);
+    bogosort(a,N);
+    // Prints already sorted absolute values
     for(int i=0; i<N; ++i)
-    {
         cout<<a[i].AbsoluteValue()<<endl;
-    }
     return 0;
 }
